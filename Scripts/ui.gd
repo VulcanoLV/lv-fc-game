67cf9eeb10994ee151d26f0cc3a800cce507d566
VulcanoLV <luke@vulcano.com.au>
2024-08-31 00:10:27 +1000

    Revert "Merge branch 'main' of https://github.com/VulcanoLV/lv-fc-game"
    This reverts commit 6fb4fe6224f4104a7eed89ea0b28b0e5b88f6250, reversing
    changes made to 2be580553dc68fa8db4a07f099711bc364d0c0f6.

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -3,4 +3,4 @@ extends Control
 @onready var inventory = $ColorRect
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_inventory"):
-		$Inventory.visible = !$Inventory.visible
+		inventory.visible = !inventory.visible
