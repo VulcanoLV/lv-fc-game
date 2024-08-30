2be580553dc68fa8db4a07f099711bc364d0c0f6
VulcanoLV <luke@vulcano.com.au>
2024-08-31 00:00:34 +1000

    fixed some stuff added some other
    improved moving script
    added dodge
    added player rotation with mouse
    added janky player sprite
    TODO:
    ui rotates with player fix that

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -1,6 +1,6 @@
 extends Control
 
-
+@onready var inventory = $ColorRect
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_inventory"):
-		$ColorRect.visible = !$ColorRect.visible
+		inventory.visible = !inventory.visible
