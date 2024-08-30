9f049a7c7b5643bb57d709df7b9171cd7b9f053a
BodeLizard <149684082+BodeLizard@users.noreply.github.com>
2024-08-30 18:08:16 +1000

    initial slot code
    changed resolution and window stretching,
    created slot class and basic hover functionality,
    added custom nine patch rect.

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -3,4 +3,4 @@ extends Control
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_inventory"):
-		$ColorRect.visible = !$ColorRect.visible
+		$Inventory.visible = !$Inventory.visible
